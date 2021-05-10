d0c0e1f17cbd6f7cd3bc1445dc8e80f0384ba3f2
tscherrer <timot.scherrer@gmail.com>
2021-05-10 11:33:05 +0200

    Configure DOM testing

--- assets/js/__mocks__/dataSource.js
+++ assets/js/__mocks__/dataSource.js
@@ -1,7 +1,12 @@
 const DataSource = jest.createMockFromModule('../DataSource')
 
-function get(action, parameters) { return new Promise((resolve, reject) => { }) }
+let __internalData = {};
+
+function get(action, parameters) { return new Promise((resolve, reject) => { resolve(__internalData) }) }
+
+function __setData(data) { __internalData = data }
 
 DataSource.get = get
+DataSource.__setData = __setData
 
-export { DataSource }
+export { DataSource, __setData }
--- assets/js/tests/dom.test.js
+++ assets/js/tests/dom.test.js
@@ -1,12 +1,21 @@
 import React from 'react'
-import { cleanup, fireEvent, render } from '@testing-library/react'
+import { act, cleanup, render, unmountComponentAtNode } from '@testing-library/react'
 import Game from '../game'
+import regeneratorRuntime from "regenerator-runtime"; // Needed to use 'async' keyword with tests. Tests must be async in order to be able to 'await' assertions.
 
 jest.mock('../DataSource')
 
-it('Game displays correctly', () => {
+it('Game displays with basic info', async () => {
     const gameKey = '123'
     const playerKey = '456'
-    const { queryByText } = render(<Game gameKey={gameKey} playerKey={playerKey}></Game>)
-    expect(queryByText("Votre équipe :")).toBeDefined()
+    let mock = require('../DataSource')
+    mock.__setData({
+        gameKey: gameKey,
+        playerName: 'Inspecteur LeBlanco',
+        cards: []
+    })
+
+    const { queryByText, findByText } = render(<Game gameKey={gameKey} playerKey={playerKey}></Game>)
+    expect(queryByText('Votre équipe :')).toBeDefined()
+    expect(await findByText('Inspecteur LeBlanco')).toBeDefined()
 })
